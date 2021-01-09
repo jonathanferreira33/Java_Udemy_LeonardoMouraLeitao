4a031b8c0df49ac1a1a226af69b42a6dcbd7cdf1
Jonathan Ferreira <jhouus33@gmail.com>
2021-01-09 09:17:17 -0300

    add Logicos, ternario e desafioLogico

---
+++ java_exercicios/src/java_exercicios/Ternario.java
@@ -0,0 +1,11 @@
+package java_exercicios;
+
+public class Ternario {
+	public static void main(String[] args) {
+		
+		double media = 8.8;
+		String resultado = media >= 7.6 ? "Aprovado" : "Recuperacao"; //primeiro resultado quando true e o segundo é quando false
+		System.out.println(resultado);
+	}
+
+}
---
+++ java_exercicios/src/java_exercicios/TipoStringEquals.java
@@ -0,0 +1,23 @@
+package java_exercicios;
+
+import java.util.Scanner;
+
+public class TipoStringEquals {
+	public static void main(String[] args) {
+		String s1 = new String("2");
+		System.out.println("2" == s1); // false, fois há comparações internas da linguagem
+		
+		
+		String s2 = new String("3");
+		System.out.println("3".equals(s2)); // true, pois o equals está comparando apenas os conteudos
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String s3 = entrada.next(); // caso utilizar nextLine, há necessidade do .trim(), .next() retira os espaços em brancos
+		System.out.println("4" == s3.trim()); // .trim() retira os espaços em branco
+		
+		
+		entrada.close();
+	}
+
+}
